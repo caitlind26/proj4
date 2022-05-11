6ae25c1f7446bd34d62326cc64b81f4219f6fb87
Caitlin D'Souza <cd394@njit.edu>
2022-05-10 20:57:25 -0400

    cleared db

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-
-from sqlalchemy import Integer, ForeignKey
 from sqlalchemy.orm import relationship
+
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.db import db
 from flask_login import UserMixin
